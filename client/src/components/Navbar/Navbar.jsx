8af03797e1c3289ee77e23bf894056685d27bedf
aayush <kaayush163@gmail.com>
2025-07-06 11:53:47 +0530

    Navbar hoverlink and active hover

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,43 @@
-import React from "react";
-import { assets } from "../../assets";
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact-us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <Link to="/cart" className="navbar-search-icon">
+        <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
-          <div className={getTotalCartAmount() > 0 ? "dot" : ""}></div>
-        </Link>
+        </div>
       </div>
     </div>
   );
