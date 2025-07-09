6b63dfb11768d4f2fa3bfc9dafb2c477a40e83b9
aayush <kaayush163@gmail.com>
2025-07-09 09:41:36 +0530

    ContextAPI Cartaddition deletion logic

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -3,8 +3,25 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
   return (
     <StoreContext.Provider value={contextValue}>
