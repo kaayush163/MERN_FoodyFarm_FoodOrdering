b927acc702f63c189689fc34d3ec6f42479ba0c9
aayush <kaayush163@gmail.com>
2025-07-26 06:43:13 +0530

    Context API for getTotalCartAmount

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -17,9 +17,22 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  // useEffect(() => {
+  //   console.log(cartItems);
+  // }, [cartItems]);
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      try {
+        if (cartItems[item] > 0) {
+          let itemInfo = food_list.find((product) => product._id === item);
+          totalAmount += itemInfo.price * cartItems[item];
+        }
+      } catch (error) {}
+    }
+    return totalAmount;
+  };
 
   const contextValue = {
     food_list,
