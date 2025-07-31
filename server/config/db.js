7c6c6da4b45927d68d260f216868894ef5972314
aayush <kaayush163@gmail.com>
2025-07-31 09:25:41 +0530

    export connectDB from db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    await (
+      await mongoose.connect(`process.env.MONGO_URI/${foodyfarm}`)
+    ).then(() => {
+      console.log("DB Connected!!!");
     });
-    console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1); // Exit process with failure
