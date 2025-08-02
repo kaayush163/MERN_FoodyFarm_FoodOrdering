909f359eb2e2ec6240d0892ed726e83ae165ee4e
aayush <kaayush163@gmail.com>
2025-08-02 15:28:16 +0530

    image multer used on routes Addfood

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -6,6 +6,15 @@ import multer from "multer";
 
 const foodRouter = express.Router();
 
-foodRouter.post("/add", addFood);
+// Image storage Engine
+
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
+foodRouter.post("/add", upload.single("image"), addFood);
 
 export default foodRouter;
