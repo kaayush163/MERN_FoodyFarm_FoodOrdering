18a638abdf21a076d53ac6596e8e7c3141537e7a
aayush <kaayush163@gmail.com>
2025-08-02 15:12:06 +0530

    addFood building started on controller,routes and server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoutes.js";
 
 // app config
 const app = express();
@@ -12,6 +13,9 @@ app.use(cors());
 
 // Db connection
 connectDB();
+
+// api endpoints
+app.use("/api/foods", foodRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
