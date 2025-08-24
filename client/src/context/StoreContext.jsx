34c345edd65a5a45c0fe3a0d054ed82e900c78ef
aayush <kaayush163@gmail.com>
2025-08-24 12:00:25 +0530

    use Effect loadCartData fucntion call added

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -72,6 +72,7 @@ const StoreContextProvider = (props) => {
       await fetchFoodList();
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"));
       }
     }
     loadData();
