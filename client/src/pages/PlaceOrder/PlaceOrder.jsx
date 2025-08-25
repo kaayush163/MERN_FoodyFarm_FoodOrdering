d91dcd2f1d1dfca2579a5db4933b96acde53644a
aayush <kaayush163@gmail.com>
2025-08-25 10:50:53 +0530

    onChangeHandler function ready on PlaceOrder.jsx

--- client/src/pages/PlaceOrder/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -17,6 +17,12 @@ const PlaceOrder = () => {
     phone: "",
   });
 
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
   return (
     <form action="" className="place-order">
       <div className="place-order-left">
