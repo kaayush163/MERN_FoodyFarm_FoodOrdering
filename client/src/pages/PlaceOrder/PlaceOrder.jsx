3310c0999417fbc28a2ec96bc4ed2cfcbde80b50
aayush <kaayush163@gmail.com>
2025-08-26 09:54:41 +0530

    placeOrder form submitting function calling

--- client/src/pages/PlaceOrder/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -36,6 +36,46 @@ const PlaceOrder = () => {
   //   }
   // }, [token]);
 
+  const placeOrder = async (e) => {
+    e.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    console.log(orderItems);
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + deliveryCharge,
+    };
+    if (payment === "stripe") {
+      let response = await axios.post(url + "/api/order/place", orderData, {
+        headers: { token },
+      });
+      if (response.data.success) {
+        const { session_url } = response.data;
+        window.location.replace(session_url);
+      } else {
+        toast.error("Something Went Wrong");
+      }
+    } else {
+      let response = await axios.post(url + "/api/order/placecod", orderData, {
+        headers: { token },
+      });
+      if (response.data.success) {
+        navigate("/myorders");
+        toast.success(response.data.message);
+        setCartItems({});
+      } else {
+        toast.error("Something Went Wrong");
+      }
+    }
+  };
+
   return (
     <form onSubmit={placeOrder} className="place-order">
       <div className="place-order-left">
