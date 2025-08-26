933bec824dad123e6da30d66f3992da47e92c232
aayush <kaayush163@gmail.com>
2025-08-26 12:19:06 +0530

    Verify Order functionality created in controllers of order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -56,3 +56,18 @@ const placeOrder = async (req, res) => {
     res.json({ success: false, message: "Error" });
   }
 };
+
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: "Not  Verified" });
+  }
+};
