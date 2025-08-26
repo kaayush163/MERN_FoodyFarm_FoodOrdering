45d844e9387655a301e5d743d0b91aa0863f577c
aayush <kaayush163@gmail.com>
2025-08-27 03:37:25 +0530

    listOrder functionality added in order controller

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./Add.css";
 import { assets } from "../../assets/assets";
 import axios from "axios";
 import { toast } from "react-toastify";
