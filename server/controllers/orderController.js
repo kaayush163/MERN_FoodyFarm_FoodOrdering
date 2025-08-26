21f93f546443cc45d5c6459b5b489a16da0c660f
aayush <kaayush163@gmail.com>
2025-08-26 19:13:54 +0530

    useOrders added in order controller

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -71,3 +71,16 @@ const verifyOrder = async (req, res) => {
     res.json({ success: false, message: "Not  Verified" });
   }
 };
+
+// user orders for frontend
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders };
